9597e15edabf419fd8876300ac6f6b5107691423
sabbir <alam.sabbir78692@gmail.com>
2023-11-04 21:27:13 +0530

    changes

--- routes/routes.js
+++ routes/routes.js
@@ -14,7 +14,7 @@ app.get("/health", (req, res) => {
 
 const otplim = limit({
   windowMs: 5 * 60 * 1000,
-  max: 5,
+  max: 20,
   handler: async (req, res) => {
     try {
       const ipAddress = requestIP.getClientIp(req);
